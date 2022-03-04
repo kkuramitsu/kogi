fb77c4caed45345e3538f3e59fae32a433e2cb75
kkuramitsu <kkuramitsu@gmail.com>
2022-03-04 20:27:46 +0900

    rename kogi

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ def _requires_from_file(filename):
     return open(filename).read().splitlines()
 
 
-setup(name="corgi",
+setup(name="kogi",
       version="0.1",
-      description="Corgi Programming AI",
-      url="https://github.com/kkuramitsu/corgi",
+      description="Kogi Programming AI",
+      url="https://github.com/kkuramitsu/kogi",
       packages=find_packages("src"),
       package_dir={"": "src"},
-      package_data={'corgi': ['data/*.txt']},
+      package_data={'kogi': ['data/*.txt']},
       install_requires=_requires_from_file('requirements.txt'),
       )
