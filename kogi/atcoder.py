131674d6fddd159d914f1f59bcacb5bc3e920f0b
kkuramitsu <kkuramitsu@gmail.com>
2022-03-07 05:31:15 +0900

    atcoder

--- kogi/atcoder.py
+++ kogi/atcoder.py
@@ -1,6 +1,6 @@
 
 from IPython.core.magic import register_cell_magic
-from .runner import kogi_add_option
+from .runner import kogi_run, kogi_add_option
 from .atcoder_impl import print, input, _check_atcoder, _run_judge
 
 kogi_add_option(_check_atcoder, _run_judge)
@@ -8,4 +8,4 @@ kogi_add_option(_check_atcoder, _run_judge)
 
 @register_cell_magic
 def atcoder(option, code):
-    _run_judge(code, option)
+    kogi_run(code, option, run_cell=_run_judge)
