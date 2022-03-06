0003527cd5160bebd6084afefc94b7a9ba597617
kkuramitsu <kkuramitsu@gmail.com>
2022-03-06 18:07:18 +0900

    kogi

--- kogi/__init__.py
+++ kogi/__init__.py
@@ -1,6 +1,6 @@
 from IPython.core.magic import register_cell_magic
 
-from .chat import kogi_help, kogi_translate
+from .webui import kogi_help, kogi_translate, kogi_login
 from .runner import kogi_run, kogi_add_option
 from .atcoder import input, print, _check_atcoder, _run_judge
 
