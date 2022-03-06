c00fe740b2445a77af3ac4c06c8be8749c8b2e31
kkuramitsu <kkuramitsu@gmail.com>
2022-03-06 17:47:05 +0900

    kogi_login

--- kogi/runner.py
+++ kogi/runner.py
@@ -1,17 +1,16 @@
 from .errors import kogi_check_error
 
-
-def _run_cell(code, option):
-    res = get_ipython().run_cell(code)
-    res.raise_error()
-
-
 KOGI_OPTIONS = [
 ]
 
 
 def kogi_add_option(isOption, runner):
     KOGI_OPTIONS.append((isOption, runner))
+
+
+def _run_cell(code, option):
+    res = get_ipython().run_cell(code)
+    res.raise_error()
 
 
 def _kogi_check_option(option):
@@ -28,18 +27,19 @@ def kogi_run(code, option, run_cell=None):
         run_cell(code, option)
     except:
         results = kogi_check_error(code, return_html=True)
-        if 'error_orig' in results:
-            update_frame = {
-                'reason': results.get('reason', None),
-                'solution': results.get('solution', None),
-                'hint': results.get('hint', None),
-            }
-            kogi_chat(results['error'], chat=response_simply,
-                      update_frame=update_frame)
-        else:
-            update_frame = {
-                'reason': '限界しか感じません',
-                'solution': 'プログラミングが得意な新しい友達を作ろう',
-                'hint': None,
-            }
-            kogi_chat('くーん', chat=response_simply, update_frame=update_frame)
+        print(results)
+        # if 'error_orig' in results:
+        #     update_frame = {
+        #         'reason': results.get('reason', None),
+        #         'solution': results.get('solution', None),
+        #         'hint': results.get('hint', None),
+        #     }
+        #     kogi_chat(results['error'], chat=response_simply,
+        #               update_frame=update_frame)
+        # else:
+        #     update_frame = {
+        #         'reason': '限界しか感じません',
+        #         'solution': 'プログラミングが得意な新しい友達を作ろう',
+        #         'hint': None,
+        #     }
+        #     kogi_chat('くーん', chat=response_simply)
--- kogi/webui.py
+++ kogi/webui.py
@@ -1,3 +1,6 @@
+import json
+import uuid
+import os
 import IPython
 from IPython.display import display, HTML
 from google.colab import output
@@ -390,3 +393,156 @@ def kogi_translate(delay=600, print=print_nop):
     display(IPython.display.HTML(TRANSLATE_CSS_HTML))
     SCRIPT = TRANSLATE_SCRIPT.replace('600', str(delay))
     display(IPython.display.HTML(SCRIPT))
+
+
+# LOGIN
+
+
+# ダミー関数
+LOGIN_HTML = '''
+<style>
+.parent {
+  background-color: #edebeb;
+  width: 100%;
+  height: 150px;
+}
+textarea {
+  width: 100%; 
+  box-sizing: border-box;  /* ※これがないと横にはみ出る */
+  height:120px; 
+  font-size: large;
+  outline: none;           /* ※ブラウザが標準で付加する線を消したいとき */
+  resize: none;
+}
+.box18{
+  //padding: 0.2em 0.5em;
+  //margin: 2em 0;
+  color: #565656;
+  background: #ffeaea;
+  //box-shadow: 0px 0px 0px 10px #ffeaea;
+  border: dashed 2px #ffc3c3;
+  //border-radius: 8px;
+}
+.box16{
+    //padding: 0.5em 1em;
+    //margin: 2em 0;
+    background: -webkit-repeating-linear-gradient(-45deg, #f0f8ff, #f0f8ff 3px,#e9f4ff 3px, #e9f4ff 7px);
+    background: repeating-linear-gradient(-45deg, #f0f8ff, #f0f8ff 3px,#e9f4ff 3px, #e9f4ff 7px);
+}
+.box24 {
+    position: relative;
+    padding: 0.5em 0.7em;
+    margin: 2em 0;
+    background: #6f4b3e;
+    color: white;
+    font-weight: bold;
+}
+.button02 {
+  //justify-content: space-between;
+  //margin: 0 auto;
+  //padding: 1em 2em;
+  width: 300px;
+  color: #333;
+  //font-size: 18px;
+  font-weight: 700;
+  background-color: #cccccc;
+  border-radius: 50vh;
+}
+</style>
+<label>Student ID</label><input id="name"/>
+<span class="button02" id="ok">Ready</span>
+<div class="parent">
+<div style="float: left; width: 48%; text-align: right;">
+<label class="box24" for="input">Type In</label>
+<textarea id="input" class="box16"></textarea>
+</div>
+<div style="float: left; width: 48%; text-align: right;">
+<label class="box24" for="outout">Code</label>
+<textarea id="output" class="box18 python" readonly>print(math.sin(math.pi/2))
+print(["oranges", "tables"])
+print(weight / (height * height))
+print(x if x >= y else y)
+print(s[0].upper() for s in "abc")
+</textarea>
+</div>
+</div>
+'''
+
+LOGIN_SCRIPT = '''
+<script>
+    var timer = null;
+    var buffers = [];
+    var dict = {};
+    const idPane = document.getElementById('name');
+    const inputPane = document.getElementById('input');
+    var submitted = false;
+    var buttonClick = () => {
+        if(!submitted) {
+            var name = idPane.value;
+            var value = inputPane.value;
+            var text = buffers.join(' ');
+            google.colab.kernel.invokeFunction('notebook.login', [name, value, dict, text, window.navigator.userAgent], {});
+            submitted = true;
+        }
+    };
+    var before = new Date().getTime();
+    idPane.addEventListener('keydown', (e) => {
+        before = new Date().getTime();
+        if(idPane.value.length >= 7) {
+            document.getElementById('ok').innerText = 'Go';
+            return;
+        }
+    });
+    inputPane.addEventListener('keydown', (e) => {
+      var now = new Date().getTime();
+      if(e.key === ' ') {
+        buffers.push(`${now - before} SPACE`);
+      }
+      else {
+        buffers.push(`${now - before} ${e.key}`);
+      }
+      before = now;
+      if(idPane.value.length < 7) {
+          inputPane.value = '';
+          return;
+      }
+      dict[e.key] = (dict[e.key] || 0) + 1;
+      var size = inputPane.value.length;
+      if(size > 10 && dict[')'] >= 8 && dict['i'] >= 10 && dict['t'] >= 10) {
+        document.getElementById('ok').innerText = '出席';
+        setTimeout(buttonClick, 5000);
+      }
+      else{
+        document.getElementById('ok').innerText = `${size}`;
+      }
+    });
+</script>
+'''
+
+
+def print_nop(*x):
+    pass
+
+
+def kogi_login(print=print_nop):
+    def login(name, code, counts, keys, useragent):
+        try:
+            code = code.strip()
+            keys = keys.split('\n')[-1]
+            jsondata = {
+                'name': name,
+                'code': code,
+                'keys': keys,
+                'counts': counts,
+                'browser': useragent,
+            }
+            print(jsondata)
+        except Exception as e:
+            print(e)
+
+    output.register_callback('notebook.login', login)
+    display(IPython.display.HTML(LOGIN_HTML))
+    display(IPython.display.HTML(LOGIN_SCRIPT))
+
+
+kogi_login()
