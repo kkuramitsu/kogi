80dd68fa9d3d0e30284cf05414b3baaa64991137
kkuramitsu <kkuramitsu@gmail.com>
2022-05-02 11:30:51 +0900

    fix: __init__.py

--- kogi/__init__.py
+++ kogi/__init__.py
@@ -1,12 +1,13 @@
 
-try:
-    from .webui import kogi_login, kogi_help, kogi_translate, kogi_print
-except ModuleNotFoundError:
-    pass
-
+# try:
+#     from .webui import kogi_login, kogi_help, kogi_translate, kogi_print
+# except ModuleNotFoundError:
+#     pass
 #from .runner import kogi, corgi
+
 
 try:
     from .exception_hook import enable_kogi_hook, disable_kogi_hook
+    enable_kogi_hook()
 except ModuleNotFoundError:
     pass
