afe5ebb4134e0cc974cd338c313d55b9c405746a
kkuramitsu <kkuramitsu@gmail.com>
2022-05-11 12:25:55 +0900

    fix: error

--- kogi/parse_error.py
+++ kogi/parse_error.py
@@ -7,7 +7,7 @@ import traceback
 
 # from .render_html import render
 
-from .parse_code import parse_find_name, parse_find_app, parse_find_index, parse_find_infix
+from parse_code import parse_find_name, parse_find_app, parse_find_index, parse_find_infix
 
 DEFINED_ERRORS = []
 
@@ -22,6 +22,7 @@ def KOGI_ERR(**kw):
             ',')) if ',' in defined['keys'] else (defined['keys'],)
     DEFINED_ERRORS.append(defined)
 
+
 def DEBUG_ERR(**kw):
     global DEFINED_ERRORS
     defined = dict(**kw)
@@ -30,7 +31,7 @@ def DEBUG_ERR(**kw):
     if 'keys' in defined:
         defined['keys'] = tuple(defined['keys'].split(
             ',')) if ',' in defined['keys'] else (defined['keys'],)
-    DEFINED_ERRORS = [defined] + DEFINED_ERRORS
+    DEFINED_ERRORS = [defined]
 
 
 # KOGI 定義
@@ -52,6 +53,7 @@ MODULE_NAME = {
     'sin': 'math',
     'cos': 'math',
 }
+
 
 def check_name(slots, line):
     name = slots['matched']['name']
@@ -108,26 +110,56 @@ def test_NULL():
 KOGI_ERR(
     pattern='name \'(.*?)\' is not defined',
     keys='name',
-    translated='名前エラーです。\n{name}という名前を使いましたが、未定義です。\nつまり使える状態ではありません',
+    translated='名前エラーです。\n{name}という名前を使いましたが、未定義です。',
+    reason='{name}は、値が代入もされておらず、インポートもされていません',
+    hint='{name}の単なる打ち間違いかも',
+    solution='{name}の種類（変数、関数名、モジュール名）を確認しましょう',
+    terms={
+        '未定義': '変数名に値や関数が与えられておらず、使える状態ではないこと'
+    },
     check=check_name,
-    reason='{name}の単なる打ち間違いかも',
-    hint='{name}の種類（変数、関数名、モジュール名）を確認しましょう',
-    solution='{name}の種類によって解決法は異なります',
     test=(test_NameError, test_NameError2, test_NULL),
 )
 
+'''
+s = int(input().split(\", \"))\n",
+      "TypeError: int() argument must be a string, a bytes-like object or a number, not 'list'\n"
 
-def check_recv(slots, line):
-    name = slots['matched']['name']
-    type = slots['matched']['type']
-    slots['line'] = line
-    ss = parse_find_name(line, name, defined_key='recv')
-    if len(ss) == 1:
-        recv = ss[0]['recv']
-        id = 'メソッド' if ss[0]['ctype'] == 'method' else 'プロパティ'
-        slots['translated'] = f'型エラー: {recv}は{type}型です。{name}のような{id}はありません'
-        slots['reason'] = f'{name}の打ち間違い、もしくは{recv}の{type}型が想定外'
-        slots['hint'] = f'{recv}にが想定通りの型となるように修正してください'
+KeyboardInterrupt
+NameError: name 'math' is not defined
+
+IndexError: string index out of range
+
+TypeError: map() must have at least two arguments.
+TypeError: range expected 1 arguments, got 0
+
+ModuleNotFoundError: No module named 'Levenshtein'
+int.input()
+"AttributeError: type object 'int' has no attribute 'input'"
+NameError: name 'Ture' is not defined
+ValueError: not enough values to unpack (expected 2, got 1)
+"TypeError: 'builtin_function_or_method' object is not iterable"
+ValueError: '1' is not in list
+
+"error_orig": "AttributeError: 'list' object has no attribute 'replace'"
+"SyntaxError: keyword can't be an expression\n"
+SyntaxError: keyword can't be an expression キーワード引数
+
+"error_type": "UnboundLocalError",
+      "error_orig": "UnboundLocalError: local variable 's' referenced before assignment",
+      "code": "\n\n\nN, K = map(int, input().split())\n\n# def g1( x ) :\n#   l1 = []\n#   for i in range( len(str(x)) ) :\n#     l1.append( int( str(x[ i ]) ) )\n#   l2 = sorted(l1, reverse = False)\
+n#   for j in range( len(str(x)) ) :\n#     s += l[ j ] * (10 ** j )\n#   return s\n\n\n\ndef g1( x ) :\n  X = str(x)\n  l1 = []\n  for i in range( len( X ) ) :\n    l1.append( str( X[ i ] ) )\n  l2 =
+ sorted(l1, reverse = False)\n  for j in range( len( X ) ) :\n    s += l[ j ] * (10 ** j )\n  return s\n\n\n\n\nA = 314\nprint(g1(A))",
+
+"TypeError: object of type 'int' has no len()"
+ TypeError: list indices must be integers or slices, not map
+"IndexError: list assignment index out of range"
+TypeError: 'str' object does not support item assignment
+TypeError: 'int' object does not support item assignment
+TypeError: can only concatenate str (not \"int\") to str
+'''
+
+# AttributeError: 'NoneType' object has no attribute 'append'
 
 
 def test_NoAttribute():
@@ -140,11 +172,32 @@ def test_NoMethod():
     print(d.a())
 
 
+def test_NoneMethod():
+    d = None
+    print(d.append())
+
+
+def check_recv(slots, line):
+    name = slots['matched']['name']
+    type = slots['matched']['type']
+    slots['line'] = line
+    ss = parse_find_name(line, name, defined_key='recv')
+    if len(ss) == 1:
+        recv = ss[0]['recv']
+        id = 'メソッド' if ss[0]['ctype'] == 'method' else 'プロパティ'
+        slots['translated'] = f'型エラーです\n{recv}は{type}型です。{name}のような{id}はありません'
+        if type == 'NoneType':
+            slots['reason'] = f'{recv}の値がNoneになっています'
+        else:
+            slots['reason'] = f'{name}の打ち間違い\nもしくは{recv}の{type}型が想定したものと異なります'
+        slots['hint'] = f'{recv}が想定通りの型となるように修正してください'
+
+
 KOGI_ERR(
     pattern='\'(.*?)\' object has no attribute \'(.*?)\'',
     keys='type,name',
     translated='型エラー: レシーバは{type}型です。{name}のようなメソッドやプロパティはありません.',
-    reason='{name}の打ち間違い、もしくはレシーバの型{type}が想定外',
+    reason='{name}の打ち間違い\nもしくはレシーバの型{type}が想定したものと異なります',
     hint='レシーバが想定通りの型となるように修正してください',
     check=check_recv,
     test=(test_NoAttribute, test_NoMethod),
@@ -160,8 +213,8 @@ KOGI_ERR(
     pattern='module \'(.*?)\' has no attribute \'(.*?)\'',
     keys='module,name',
     translated='{module}モジュールには、{name}のような関数やプロパティはありません',
-    reason='{name}の打ち間違いか、もしくは{module}を別のモジュールと勘違いしている',
-    hint='dir({module})で利用できる名前を調べてみよう.',
+    reason='{name}の打ち間違い\nもしくは{module}を別のモジュールと勘違い',
+    hint='`dir({module})`で利用できる関数やクラスを確認しよう.',
     test=test_ModuleNoAttribute,
 )
 
@@ -174,9 +227,25 @@ KOGI_ERR(
     pattern='ImportError: cannot import name \'(.*?)\' from \'(.*?)\'',
     keys='name,module',
     translated='{module}モジュールには、{name}のような関数やプロパティはありません',
-    reason='{name}の打ち間違いか、もしくは{module}を別のモジュールと勘違いしている',
-    hint='dir({module})で利用できる名前を調べてみよう.',
+    reason='{name}の打ち間違い\nもしくは{module}を別のモジュールと勘違い',
+    hint='`dir({module})`で利用できる関数やクラスを確認しよう.',
     test=test_ImportError,
+)
+
+# TypeError: 'str' object cannot be interpreted as an integer
+
+
+def test_AsInteger():
+    return range('100')
+
+
+KOGI_ERR(
+    pattern='TypeError: \'(.*?)\' object cannot be interpreted as an integer',
+    keys='type',
+    translated='型エラーです\n{type}型の値は整数として扱えません.',
+    reason='整数値を与えるべきところで、{type}型の値を与えました.',
+    hint='整数に変換するといいかも',
+    test=test_AsInteger,
 )
 
 
@@ -187,12 +256,15 @@ def test_UnsupportedOperand():
 KOGI_ERR(
     pattern='unsupported operand type\(s\) for (.*?): \'(.*?)\' and \'(.*?)\'',
     keys='name,type,type2',
-    translated='{type}と{type2}の間で計算しようとしたけど、そこでは演算子{name}は使えません.',
-    solution='{type}か{type2}のどちらかの型に変換するといいかも',
+    translated='型エラーです\n{type}と{type2}の間では、演算子{name}は使えません.',
+    hint='適切に型を変換するといいかも',
     # inspect=inspect_infix,
     error_ext='{left}と{right}の間で計算しようとしたけど、型がそれぞれ{type}と{type2}で異なり、演算子{name}は使えません.',
     test=test_UnsupportedOperand,
 )
+
+
+# TypeError: '<=' not supported between instances of 'int' and 'str'
 
 KOGI_ERR(
     pattern='\'(.*?)\' not supported between instances of \'(.*?)\' and \'(.*?)\'',
@@ -203,12 +275,46 @@ KOGI_ERR(
     error_ext='{left}と{right}の間で計算しようとしたけど、型がそれぞれ{type}と{type2}で異なり、演算子{name}は使えません.',
 )
 
+# TypeError: 'in <string>' requires string as left operand, not int
+
+
+def test_InStr():
+    return 9 in '99'
+
+
+KOGI_ERR(
+    pattern='\'in \\<string\\>\' requires (string) as left operand',
+    keys='dummy',
+    translated='`in <文字列>`の左辺は文字列でなければなりません',
+    hint='演算子の左辺を文字列にします',
+    test=test_InStr,
+)
+
+# TypeError: argument of type 'int' is not iterable
+
+
+def test_NotIterableIn():
+    return 9 in 99
+
+
+KOGI_ERR(
+    pattern='argument of type \'(.*?)\' is not iterable',
+    keys='type',
+    translated='{type}はイテラブルではありません',
+    terms={
+        'イテラブル', '文字列やリスト、タプルのように値が並んだもの'
+    },
+    test=test_NotIterableIn,
+)
+
+# TypeError: replace() argument 1 must be str, not int
+
 
 def check_callable(slots, lines):
     import builtins
     type = slots['matched']['type']
     ss = parse_find_app(lines)
-    #print(ss)
+    # print(ss)
     if len(ss) == 1 and 'cname' in ss[0]:
         name = ss[0]['cname']
         slots['translated'] = f'{name}の値は{type}型の値が代入されています。\n関数ではありません'
@@ -216,6 +322,7 @@ def check_callable(slots, lines):
         slots['hint'] = f'{name}を何とかして関数に戻します'
         if name in dir(builtins):
             slots['solution'] = f'`from buitins import {name}`を試して'
+
 
 def test_NotCallable():
     a = 1
@@ -231,6 +338,9 @@ KOGI_ERR(
     check=check_callable,
     test=test_NotCallable,
 )
+
+# "TypeError: 'type' object is not iterable"
+# for _ in range: pass
 
 
 def test_NotIterable():
@@ -270,8 +380,8 @@ KOGI_ERR(
     test=test_MustBeNoneOr,
 )
 
-#render_value() missing 1 required positional argument: 'value'
-#TypeError: render_value() missing 2 required positional arguments: 'typename' and 'value'
+# render_value() missing 1 required positional argument: 'value'
+# TypeError: render_value() missing 2 required positional arguments: 'typename' and 'value'
 
 KOGI_ERR(
     pattern='(\\S+?)\\(\\) missing (\\d+) required positional arguments?: (.*)$',
@@ -279,8 +389,9 @@ KOGI_ERR(
     translated='型エラーです\n{name}には、{num}個の引数が足りません',
     reason='足りない引数は、{arguments}です。',
     solution='{name}の定義をよくみてください',
-    #check=check_arguments,
+    # check=check_arguments,
 )
+
 
 def test_InvalidKeyword():
     print(color='red')
@@ -353,15 +464,28 @@ KOGI_ERR(
     solution='エラーの発生した行の前後を含めて、構文エラーを探して！',
 )
 
+# ValueError: invalid literal for int() with base 10: '10 3'
+
 
 def test_InvalidLiteral():
     int('a')
 
 
+def check_base(slots, lines):
+    value = slots['matched']['value']
+    try:
+        if len(list(map(int, value.split(' ')))) > 0:
+            slots['hint'] = '%%atcoderの問題番号が間違っているかも',
+    except:
+        pass
+
+
 KOGI_ERR(
-    pattern='invalid literal for int\(\) with base (.*?): (\'.*?\')',
+    pattern='invalid literal for int\\(\\) with base (.*?): (\'.*?\')',
     keys='base,value',
-    translated='文字列{value}は、整数に変換できる文字列ではありません',
+    translated='文字列{value}は、整数値に変換できません',
+    hint='入力データも確認してください',
+    check=check_base,
     test=test_InvalidLiteral,
 )
 
@@ -435,14 +559,14 @@ def test_DividedByZero():
 KOGI_ERR(
     pattern='division by (zero)',
     keys='dummy',
-    translated='ゼロで徐算しました. ',
-    reason='ゼロで割ることはできません. ',
-    hint='分母の値を確認してみましょう.',
+    translated='ゼロで徐算してしまいました. ',
+    reason='数式はゼロで割ることはできません. ',
+    hint='分母の値がゼロにならないようにしましょう.',
     test=test_DividedByZero
 )
 
 
-def test_FileNotFoundError():
+def _test_FileNotFoundError():
     open('file')
 
 
@@ -450,9 +574,9 @@ KOGI_ERR(
     pattern='FileNotFoundError: \\[Errno (\\d+)\\] No such file or directory: (\'.*\')',
     keys='errno,file',
     translated='{file}が見つかりません. ',
-    reason='ファイル名やファイルパスが間違っている. ',
-    hint='{file}が存在するか確認しよう.',
-    test=test_FileNotFoundError,
+    reason='指定されたファイルが存在しない.',
+    hint='{file}が存在するか、確認してみよう.',
+    test=_test_FileNotFoundError,
 )
 
 # UsageError: Cell magic `%%kogi2` not found.
@@ -478,14 +602,34 @@ KOGI_ERR(
 
 # ValueError: too many values to unpack (expected 2)
 
+def _test_TooManyUnpack():
+    a, b = [1, 2, 3]
+
+
 KOGI_ERR(
-    pattern='ValueError: too many values to unpack \\(expected (\\d.)\\)',
+    pattern='ValueError: too many values to unpack \\(expected (\\d+)\\)',
     keys='num',
-    translated='値エラー: {num}個の値を変数に展開しようとしたが、個数が不一致です。',
-    reason='リストやタプルなどの値の数が、想定通りではない',
-    hint='入力データも確認してみてください',
+    translated='リストやタプルなどから{num}個の値を変数に展開しようとしたが、個数が多すぎます',
+    reason='リストやタプルなどの値の数が、{num}個ではない',
+    hint='個数が可変なときは、この書き方はできません',
+    test=_test_TooManyUnpack,
 )
 
+# ValueError: not enough values to unpack (expected 2, got 1)
+
+
+def _test_NotEnoughUnpack():
+    a, b = [1]
+
+
+KOGI_ERR(
+    pattern='not enough values to unpack \\(expected (\\d+)',
+    keys='num',
+    translated='リストやタプルなどから{num}個の値を変数に展開しようとしたが、個数が少な過ぎます',
+    reason='リストやタプルなどの値の数が、{num}個ではない',
+    hint='個数が可変なときは、この書き方はできません',
+    test=_test_NotEnoughUnpack,
+)
 
 # error
 
@@ -559,16 +703,17 @@ def test_exception_hook():
     print(slots)
 
 
-if __name__ == '__main__':
-    def test_DEFINED_ERRORS():
-        for defined in DEFINED_ERRORS:
-            fs = defined.get('test', ())
-            if not isinstance(fs, tuple):
-                fs = (fs,)
-            for f in fs:
-                try:
-                    f()
-                except:
-                    test_exception_hook()
+def test_DEFINED_ERRORS():
+    for defined in DEFINED_ERRORS:
+        fs = defined.get('test', ())
+        if not isinstance(fs, tuple):
+            fs = (fs,)
+        for f in fs:
+            try:
+                f()
+            except:
+                test_exception_hook()
 
+
+if __name__ == '__main__':
     test_DEFINED_ERRORS()
