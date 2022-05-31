8bc47587305d17e33fed593e25ff10c5a1808412
Kimio Kuramitsu <kkuramitsu@gmail.com>
2022-05-31 14:19:15 +0900

    loader

--- kogi/nmt.py
+++ kogi/nmt.py
@@ -68,24 +68,98 @@ textarea {
     width: 0;
     height: 0;
 }
+// loader
+.loader {
+  align-items: center;
+  background: #fff;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  z-index: 999;
+  &::after {
+    animation: loader 0.5s linear infinite;
+    border: 1px solid orange;
+    border-radius: 50%;
+    border-right: 1px solid rgba(orange, 0.2);
+    border-top: 1px solid rgba(orange, 0.2);
+    content: "";
+    height: 70px;
+    width: 70px;
+  }
+}
+
+@keyframes loader {
+  0% {
+    transform: rotate(0);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+body {
+  font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", Meiryo, sans-serif;
+}
+
+main{
+  line-height: 1.6;
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 25px;
+  h1{
+    font-size: 32px;
+    font-weight: bold;
+  }
+  p{
+    font-size: 16px;
+    margin-top: 30px;
+  }
+}
 </style>
 <div class="parent">
 <div style="float: left; width: 48%; text-align: right;">
-<label class="box24" for="input">日本語</label>
-<textarea id="input" class="box16"></textarea>
+<label class="box24" for="input">INPUT</label>
+<textarea id="input" class="box16" readonly></textarea>
 </div>
 <div style="float: left; width: 48%; text-align: right;">
-<label class="box24" for="outout">Python</label>
+<label class="box24" for="outout">OUTOUT</label>
 <textarea id="output" class="box18 python" readonly></textarea>
 </div>
 </div>
+<div id="js-loader" class="loader"></div>
+<script>
+const loader = document.getElementById('js-loader');
+window.addEventListener('load', () => {
+  const ms = 4000;
+  loader.style.transition = 'opacity ' + ms + 'ms';
+  
+  const loaderOpacity = function(){
+    loader.style.opacity = 0;
+  }
+  const loaderDisplay = function(){
+    loader.style.display = "none";
+  }
+  // setTimeout(loaderOpacity, 1);
+  // setTimeout(loaderDisplay, ms);
+  // デモ用
+  setTimeout(loaderOpacity, 1000);
+  setTimeout(loaderDisplay, 1000 + ms);
+});
+</script>
 '''
 
 TRANSLATE_SCRIPT = '''
 <script>
     var timer = null;
     var logtimer = null;
-    document.getElementById('input').addEventListener('input', (e) => {
+    var inputPane = document.getElementById('input');
+    inputPane.disabled = false;
+    inputPane.addEventListener('input', (e) => {
         var text = e.srcElement.value;
         if(timer !== null) {
             clearTimeout(timer);
@@ -140,10 +214,10 @@ def load_mt5(model_id, qint8=True, device='cpu', print=print):
 
     return gready_search
 
-def translate(model_id, load_nmt=load_mt5, beam=1, device='cpu', qint8=True, print = print):
-    display(HTML(TRANSLATE_CSS_HTML))
+def translate(model_id, load_nmt=load_mt5, beam=1, device='cpu', qint8=True, input='日本語', output='Python', print = print):
+    display(HTML(TRANSLATE_CSS_HTML.replace('INPUT', input).replace('OUTPUT', output)))
     nmt = load_nmt(model_id, qint8=qint8, device=device, print=print)
-    cached = {}
+    cached = {'':''}
 
     def convert(text):
         try:
@@ -164,7 +238,6 @@ def translate(model_id, load_nmt=load_mt5, beam=1, device='cpu', qint8=True, pri
             return IPython.display.JSON({'result': text})
         except Exception as e:
             print(e)
-    display(HTML(TRANSLATE_SCRIPT))
 
     try:
         from google.colab import output
@@ -172,3 +245,4 @@ def translate(model_id, load_nmt=load_mt5, beam=1, device='cpu', qint8=True, pri
         output.register_callback('notebook.Logger', send_log)
     except Exception as e:
         print(e)
+    display(HTML(TRANSLATE_SCRIPT))
