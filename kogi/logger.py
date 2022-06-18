385043d90e53790033c5530b9ddc438b2dc1b69e
kkuramitsu <kkuramitsu@gmail.com>
2022-06-18 16:05:12 +0900

    fix: logging

--- kogi/logger.py
+++ kogi/logger.py
@@ -89,7 +89,7 @@ def send_log(right_now=True, print=kogi_print):
         headers = {'x-api-key': f'A{KEY}s'}
         r = requests.post(url, headers=headers, json=data)
         if r.status_code != 200:
-            print(data)
+            print(f'delta={delta} data={data}')
 
 
 def log(**kw):
