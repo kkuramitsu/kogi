8a94c71cc83ff791885b9978a0973bf1d69630e6
Kimio Kuramitsu <kkuramitsu@gmail.com>
2022-06-20 13:01:53 +0900

    kogi: 0.0.9

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def _requires_from_file(filename):
 
 
 setup(name="kogi",
-      version="0.0.8",
+      version="0.0.9",
       license='MIT',
       author='Kimio Kuramitsu',
       description="Kogi Programming AI",
