df8437a3de36ed3fef3848c42bbf97aee0d000b1
kkuramitsu <kkuramitsu@gmail.com>
2022-08-16 04:13:09 +0900

    debugging rmt

--- kogi/ui/_rmt.py
+++ kogi/ui/_rmt.py
@@ -2,7 +2,7 @@ import time
 import traceback
 from .content import ICON, JS, CSS
 from IPython.display import display, HTML, JSON
-from kogi.settings import kogi_log, translate_ja, model_generate, print_nop
+from kogi.settings import kogi_log, translate, model_generate, print_nop
 
 try:
     from google.colab import output as colab_output
@@ -54,6 +54,7 @@ MODULES = [
     ('datetime.', 'import datetime'),
     ('random.', 'import random'),
     ('np.', 'import numpy as np'),
+    ('scipy.', 'import scipy'),
     ('plt.', 'import matplotlib.pyplot as plt'),
     ('pd.', 'import pandas as pd'),
 ]
@@ -69,8 +70,9 @@ def check_module(code, fmt='<tt>{}</tt> してから、'):
 def codegen(code, text=''):
     if '<nl>' in code:
         code = code.replace('<nl>', '\n').replace('<tab>', '\t')
-    if text != '':
-        text = f'# {translate_ja(text)}\n'
+    if text != '' or not text.isascii():
+        text = translate(text, lang='ja_en')
+        text = f'# {text}\n'
     code = check_module(code, fmt='{}\n') + text + code
     return code
 
@@ -90,6 +92,10 @@ def rmt(input='入力', output='予測', delay=600, print=print_nop,
             ss = []
             for line in text.splitlines():
                 if line not in cached:
+                    if line.isascii():
+                        _line = translate(line, lang='en_ja')
+                        _line = transform.before(line)
+                        print(line, '=>', _line)
                     _line = transform.before(line)
                     s = time.time()
                     _translated = generate(_line)
